674fb3b6c25fb3fee01ceb9d05bea2b7fec8b1a1
edos009 <dostyutyunnik@gmail.com>
2022-11-09 16:52:22 +0200

    Add styling for the application

--- client/src/components/TickerItem/index.js
+++ client/src/components/TickerItem/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import cx from "classnames";
+import { IoArrowUpOutline, IoArrowDownOutline } from "react-icons/io5";
 
 import styles from "./TickerItem.module.scss";
 
 const TickerItem = ({
   item: { ticker, price, change, change_percent },
-  revItem,
+  prevItem,
 }) => {
+  const isPercentUp = change_percent > prevItem?.change_percent;
+  const isPercentDown = change_percent < prevItem?.change_percent;
+
   const stylesClChange = {
-    [styles.change_up_cl]: price > revItem?.price,
-    [styles.change_down_cl]: price < revItem?.price,
+    [styles.change_up_cl]: isPercentUp,
+    [styles.change_down_cl]: isPercentDown,
   };
   const stylesBgChange = {
-    [styles.change_up_bg]: price > revItem?.price,
-    [styles.change_down_bg]: price < revItem?.price,
+    [styles.change_up_bg]: isPercentUp,
+    [styles.change_down_bg]: isPercentDown,
   };
 
   const stylesChange = cx(styles.change, stylesClChange);
   const stylesChangePercent = cx(
-    styles.change_percent,
+    styles.change_percent_box,
     stylesClChange,
     stylesBgChange
   );
@@ -26,9 +30,14 @@ const TickerItem = ({
   return (
     <li className={styles.item}>
       <h2 className={styles.title}>{ticker}</h2>
-      <p className={styles.price}>{`${price}$`}</p>
-      <p className={stylesChange}>{`${change}$`}</p>
-      <p className={stylesChangePercent}>{`${change_percent}%`}</p>
+      <p className={styles.price}>{`${price} $`}</p>
+      <p className={stylesChange}>{`${change} $`}</p>
+      <div className={stylesChangePercent}>
+        {prevItem?.change_percent &&
+          change_percent !== prevItem?.change_percent &&
+          (isPercentUp ? <IoArrowUpOutline /> : <IoArrowDownOutline />)}
+        <p className={styles.change_percent}>{`${change_percent} %`}</p>
+      </div>
     </li>
   );
 };
